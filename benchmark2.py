174db343dc36934413aa8c0cf86dfa908188cde5
Maize Booker <mbooker734@gmail.com>
2023-02-09 14:00:27 -0500

    - Created benchmark2.py and copied coded from benchmark1.py and changed int variables to float.

---
+++ benchmark2.py
@@ -0,0 +1,16 @@
+import time
+
+start = time.time()
+
+benchmark_float = 0.0
+
+for i in range(10**10):
+    benchmark_float += 1.0
+for i in range(5*(10**9)):
+    benchmark_float *= 1.0
+for i in range(2*(10**9)):
+    benchmark_float /= 1.0
+
+end = time.time()
+time_taken = end - start
+print(f"Integer operation benchmark took {time_taken} seconds")
