235e1d8b55fb0e01512ff86b601a4eabb0b277ce
Maize Booker <mbooker734@gmail.com>
2023-02-09 23:08:41 -0500

    - Changed required_num to required_length. - iterated through list using enumerate for loop and read and wrote array items as required.

--- benchmark3.py
+++ benchmark3.py
@@ -2,6 +2,16 @@ import time
 
 start = time.time()
 
-required_num = (5 * (10 ** 9))
-array = [0] * required_num
+required_length = (5 * (10 ** 9))
+array = [0] * required_length
+
+for ind,num in enumerate(required_length):
+    # read array items
+    zero = num
+
+    #write array items
+    array[ind] = ind
+
+
+    
 
